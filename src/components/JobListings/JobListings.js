5cd09af9a00db7e2149fff83fda49eee33982986
Marcus Zierke <marcuszierke@gmail.com>
2019-12-01 13:33:46 +0100

    clean up

--- src/components/JobListings/JobListings.js
+++ src/components/JobListings/JobListings.js
@@ -1,59 +1,20 @@
 import React from 'react';
 import './JobListings.scss';
+import jobs from '../../jobScraping.json';
 
 const JobListings = () => {
-  const jobs = [
-    {
-      location: 'Berlin',
-      position: 'Frontend Developer',
-      description: 'Looking for a frontend developer specialized on React.js.'
-    },
-    {
-      location: 'Hamburg',
-      position: 'Android Developer',
-      description: 'Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android.'
-    },
-    {
-      location: 'Munich',
-      position: 'Software Developer',
-      description: 'Looking for a frontend developer specialized on ehöhö'
-    },
-    {
-      location: 'Berlin',
-      position: 'Frontend Developer',
-      description: 'Looking for a frontend developer specialized on React.js.'
-    },
-    {
-      location: 'Hamburg',
-      position: 'Android Developer',
-      description: 'Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android.'
-    },
-    {
-      location: 'Munich',
-      position: 'Software Developer',
-      description: 'Looking for a frontend developer specialized on ehöhö'
-    },
-    {
-      location: 'Berlin',
-      position: 'Frontend Developer',
-      description: 'Looking for a frontend developer specialized on React.js.'
-    },
-    {
-      location: 'Hamburg',
-      position: 'Android Developer',
-      description: 'Looking for a frontend developer specialized on Android. Looking for a frontend developer specialized on Android.Looking for a frontend developer specialized on Android.'
-    },
-    {
-      location: 'Munich',
-      position: 'Software Developer',
-      description: 'Looking for a frontend developer specialized on ehöhö'
-    },
-    {
-      location: 'Munich',
-      position: 'Software Developer',
-      description: 'Looking for a frontend developer specialized on ehöhö'
-    },
-  ];
+    const findColor = (city) => {
+    switch (city) {
+      case 'Berlin':
+          return { borderTop: '2px solid red'}
+      case 'Hamburg':
+          return { borderTop: '2px solid green'}
+      case 'Munich':
+          return { borderTop: '2px solid yellow'}
+      default:
+        return { borderTop: '2px solid blue'}
+    }
+  }
   
   return (
     <div className='container'>
@@ -61,16 +22,21 @@ const JobListings = () => {
         {
           jobs.map((job) => {
             return (
-              <div className='card-wrapper'>
-                <div className='card-location'>{job.location}</div>
-                <div className='card-position'>{job.position}</div>
-                <div className='card-description'>{job.description}</div>
-                <div className='card-button-wrapper'>
-                  <div className='card-information'>Please click on the link below for further information</div>
-                  <button className='card-button'>Details</button>
+              <div className="card-wrapper" style={findColor(job.location)}>
+                <div className="card-location">{job.location}</div>
+                <div className="card-position">{job.company}</div>
+                <div className="card-description">
+                  <p>{job.title}</p>
+                  <p>{job.description}</p>
+                </div>
+                <div className="card-button-wrapper">
+                  <div className="card-information">
+                    Please click on the link below for further information
+                  </div>
+                  <button className="card-button">Details</button>
                 </div>
               </div>
-            )
+            );
           })
         }
       </div>
